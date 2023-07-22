4d89a0708f9ab4e998fb68678f51ea837e4534d2
latan <latand666@gmail.com>
2023-07-22 22:07:56 +0300

    Add User class description and update README
    Added docstring to the User class in users.py file to provide extensive details about its attributes, methods, and inherited classes or methods. The purpose of this addition is to enhance code readability and better inform contributors about the User class's functions and usage.
    
    In the README, a section about SQLAlchemy and Alembic tools has been added to guide developers who aren't familiar with these tools. Additionally, a link to the Udemy course for these tools has been provided for more learning resources.

--- infrastructure/database/models/users.py
+++ infrastructure/database/models/users.py
@@ -9,6 +9,28 @@ from .base import Base, TimestampMixin, TableNameMixin
 
 
 class User(Base, TimestampMixin, TableNameMixin):
+    """
+    This class represents a User in the application.
+    If you want to learn more about SQLAlchemy and Alembic, you can check out the following link to my course:
+    https://www.udemy.com/course/sqlalchemy-alembic-bootcamp/?referralCode=E9099C5B5109EB747126
+
+    Attributes:
+        user_id (Mapped[int]): The unique identifier of the user.
+        username (Mapped[Optional[str]]): The username of the user.
+        full_name (Mapped[str]): The full name of the user.
+        active (Mapped[bool]): Indicates whether the user is active or not.
+        language (Mapped[str]): The language preference of the user.
+
+    Methods:
+        __repr__(): Returns a string representation of the User object.
+
+    Inherited Attributes:
+        Inherits from Base, TimestampMixin, and TableNameMixin classes, which provide additional attributes and functionality.
+
+    Inherited Methods:
+        Inherits methods from Base, TimestampMixin, and TableNameMixin classes, which provide additional functionality.
+
+    """
     user_id: Mapped[int] = mapped_column(BIGINT, primary_key=True, autoincrement=False)
     username: Mapped[Optional[str]] = mapped_column(String(128))
     full_name: Mapped[str] = mapped_column(String(128))
