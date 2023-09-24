cc8f06be582e2c5a3ca42b42e3aeefd6956235c0
Kostiantyn Kriuchkov <36363097+Latand@users.noreply.github.com>
2023-09-24 17:32:55 +0300

    Update base.py

--- infrastructure/database/models/base.py
+++ infrastructure/database/models/base.py
@@ -23,6 +23,3 @@ class TableNameMixin:
 
 class TimestampMixin:
     created_at: Mapped[datetime] = mapped_column(TIMESTAMP, server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(
-        TIMESTAMP, server_default=func.now(), onupdate=func.now()
-    )
